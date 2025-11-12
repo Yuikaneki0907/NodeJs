9ff2a4d96ff1996091fddcf394e4767f0f239d1e
Yuikaneki0907 <leanh1572004@gmail.com>
2025-11-12 19:52:59 +0700

    setup + helloworld

---
+++ app.ts
@@ -0,0 +1,19 @@
+//import express
+const express = require("express");
+const app = express();
+
+//khai bao cong
+const PORT = 8080
+
+app.get("/", (req, res) => {
+    res.send("helloooo") 
+})
+
+app.get("/leanh", (req, res) => {
+    res.send("leanhhhh") 
+})
+
+//chay ung dung voi cong 8080
+app.listen(8080, () => {
+    console.log(`running on port...: ${PORT}`);
+})
